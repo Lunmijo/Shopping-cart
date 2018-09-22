cdd1b95bf212882ff779d6ca79466f0902a6bd2d
root <root@localhost.localdomain>
2018-09-22 21:31:24 +0300

    Add start logic for finding elements

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,11 +9,33 @@ import { AdminComponent } from '../admin/admin.component';
 
 export class UserComponent {
   searchElement: string;
+  checkboxes = [];
+  results = [];
   getCategories() {
     const temp = new AdminComponent();
     return temp.categories;
   }
   search(searchValue) {
-    this.searchElement = searchValue;
+    this.results = [];
+    this.searchElement = '';
+    if (searchValue !== '') {
+      this.searchElement = searchValue;
+      this.results.push(searchValue);
+    }
+  }
+  addChecked(categorie: string) {
+    if (this.checkboxes.length > 0) {
+      for (let i = 0; i < this.checkboxes.length; i++) {
+        if (categorie === this.checkboxes[i]) {
+          this.checkboxes.splice(i, 1);
+          break;
+        } else {
+          this.checkboxes.push(categorie);
+          break;
+        }
+      }
+    } else {
+      this.checkboxes.push(categorie);
+    }
   }
 }
